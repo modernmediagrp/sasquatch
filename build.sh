81377a945bedaa94b63e719bbd01d037f8866c6b
Royce <modernmediagrp@users.noreply.github.com>
2023-07-05 09:01:49 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 
 # If not root, perform 'make install' with sudo
 if [ $UID -eq 0 ]; then
-    SUDO=""
+	SUDO=""
 else
-    SUDO="sudo"
+	SUDO="sudo"
 fi
 
 # Install prerequisites
 if hash apt-get &>/dev/null; then
-    $SUDO apt-get install build-essential liblzma-dev liblzo2-dev zlib1g-dev
+	$SUDO apt-get install build-essential liblzma-dev liblzo2-dev zlib1g-dev
 fi
 
 # Make sure we're working in the same directory as the build.sh script
@@ -18,7 +18,7 @@ cd $(dirname $(readlink -f $0))
 
 # Download squashfs4.3.tar.gz if it does not already exist
 if [ ! -e squashfs4.3.tar.gz ]; then
-    wget https://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz
+	wget https://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz
 fi
 
 # Remove any previous squashfs4.3 directory to ensure a clean patch/build
